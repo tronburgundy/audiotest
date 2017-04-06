eed1c4c23eb50e163d0761a747fb68f051e4b365
Joseph <jcollins3.umassd.edu>
2017-04-06 17:28:17 -0400

    updated timer

--- testcompress.py
+++ testcompress.py
@@ -9,22 +9,19 @@ parser.add_argument("-d", "--debug", dest='DEBUG', help="Debugging Mode", action
 
 global args
 args = parser.parse_args()
-
 class AudioTester:
     def __init__(self, raw):
         self.name = raw.partition('.')[0]
         self.raw = audiotools.open(raw)
 
     def compress(self, fmt_string):	
-        start = time.time()
-        if fmt_string == 'mp3':
-            self.mp3 = self.raw.convert(self.name+'.mp3', audiotools.MP3Audio)
+        if fmt_string == 'au':
+            self.au = self.raw.convert(self.name+'.au', audiotools.AuAudio)
 
 def main():
+    start = time.time()
     tester = AudioTester('music.wav')
-    tester.compress("mp3")
-    # Start timer
-    #time = timeit("tomp3()",setup="from __main__ import tomp3", number=1)
+    tester.compress("au")
     end = time.time()
     print("Time to compress: ", end-start)
     #TODO: Repeat with other algorithms
