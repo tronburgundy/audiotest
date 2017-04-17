3848d0659d194a94e687aa63c2bfcb562e32cd4c
M. Phil Lowney <mlowney@umassd.edu>
2017-04-17 18:41:41 -0400

    Make OGGencode more readable

--- matlab/OGGencode.m
+++ matlab/OGGencode.m
@@ -1,13 +1,18 @@
 function compressed = OGGencode( filename )
+
+% Get data and sampling rate from raw audiofile
 [raw, Fs] = audioread(filename);
 raw = raw(:,1);
 
-audiowrite('HH_10.ogg', raw, Fs)
-[ogg, Fs] = audioread('HH_10.ogg');
+% Compress the raw audiofile and get its data and sampling rate
+audiowrite('temp.ogg', raw, Fs)
+[ogg, Fs] = audioread('temp.ogg');
 
+% Create a WAV file from the compressed audio, which will be returned
 audiowrite('OGG_lossy.wav', ogg, Fs);
 [compressed, Fs] = audioread('OGG_lossy.wav');
 
-delete('HH_10.ogg')
-end
+% Delete Temporary File
+delete('temp.ogg')
 
+end
